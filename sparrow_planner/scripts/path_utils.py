1c5d4cd16971c87ad65dc2b68e460591c69d0bfd
dysun <dongyong.sun@derobotee.com>
2019-11-13 06:01:26 +0800

    first commit for multiple path

--- sparrow_planner/scripts/path_utils.py
+++ sparrow_planner/scripts/path_utils.py
@@ -11,27 +11,32 @@ class PathParser:
         self.path_idx = []
         self.path = []
 
-    def getPath(self, init_point, svg_file, origin,width, height, resolution, segment):
+    def loadPath(self, init_point, svg_file, origin,width, height, resolution, segment):
         xmldoc = minidom.parse(svg_file)
         path_strings = [path.getAttribute('d') for path in xmldoc.getElementsByTagName('path')]
-        ps = parse_path(path_strings[0])
         self.resolution = resolution 
         self.origin = origin
         self.height = height
-        self.path = [init_point]
+        self.segment = segment
+        self.paths = [self.svg2path(row_path) for row_path in path_strings]
+        
+        self.paths[0].insert(0, init_point)
+    def svg2path(self, row_path):
+        ps = parse_path(row_path)
+        path = []
         self.path_idx = [0]
-        self.path.append(self.point2Array(ps[0].point(0))) 
-        self.path_idx.append(1) 
+        path.append(self.point2Array(ps[0].point(0))) 
         valide_paths = [p for p in ps if p.length() > 0 ]   
-        map(lambda p : self.path.extend(self.pointsOfPath(p, segment)[1:]) , valide_paths)
-
+        map(lambda p : path.extend(self.pointsOfPath(p, self.segment)[1:]) , valide_paths)
+        return path
+        
     def pointsOfPath(self, p, segment):
         if isinstance(p, svg.path.Line):
             self.path_idx.append(self.path_idx[-1] + 1)
             return [self.point2Array(p.point(0)), self.point2Array(p.point(1))]
         
         curve =  [self.point2Array(p.point(x)) for x in np.arange(0, 1, segment/(p.length()*self.resolution))]
-        self.path_idx.append(self.path_idx[-1] + len(curve) - 1)
+        #self.path_idx.append(self.path_idx[-1] + len(curve) - 1)
         return curve
 
     def point2Array(self, point):
