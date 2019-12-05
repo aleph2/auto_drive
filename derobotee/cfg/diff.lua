a0406d1d093727ae7d4b5bc7a2b7f37e56f760c9
dongyong.sun <dongyong.sun@derobotee.com>
2019-12-05 16:32:15 +0800

    no pose drift but stil crash for add new submap

--- derobotee/cfg/diff.lua
+++ derobotee/cfg/diff.lua
@@ -19,7 +19,7 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "base_link",
+  tracking_frame = "base_footprint",
   published_frame = "odom",
   odom_frame = "odom",
   provide_odom_frame = false,
@@ -36,39 +36,39 @@ options = {
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
-  odometry_sampling_ratio = 0.1,
+  odometry_sampling_ratio = 1., 
   fixed_frame_pose_sampling_ratio = 1.,
-  imu_sampling_ratio = 1.,
+  imu_sampling_ratio = 0.005,
   landmarks_sampling_ratio = 1.,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1 
 
-TRAJECTORY_BUILDER_2D.min_range = 0.3
+--TRAJECTORY_BUILDER_2D.min_range = 0.3
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 2.
 TRAJECTORY_BUILDER_2D.use_imu_data = false
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 10
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 15
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 1e2
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 1e2
 
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.)
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 3.
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15)
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 7.
 
-POSE_GRAPH.optimization_problem.huber_scale = 1e2
+--POSE_GRAPH.optimization_problem.huber_scale = 1e2
 
 -----------------TUNE THESE PARAMETERS FOR LOW LATENCY-------------------------------
 
 ------------Global SLAM------------
 POSE_GRAPH.optimize_every_n_nodes = 100 -- Decrease
-POSE_GRAPH.global_sampling_ratio = 0.003 -- Decrease
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.4 -- Decrease
-POSE_GRAPH.constraint_builder.min_score = 0.85 -- Increase
-POSE_GRAPH.global_constraint_search_after_n_seconds = 30 -- Increase
-
+--POSE_GRAPH.global_sampling_ratio = 0.01 -- Decrease
+--POSE_GRAPH.constraint_builder.sampling_ratio = 0.01 -- Decrease
+--POSE_GRAPH.constraint_builder.min_score = 0.85 -- Increase
+--POSE_GRAPH.global_constraint_search_after_n_seconds = 30 -- Increase
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 10
 ---------Global/Local SLAM---------
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 100 -- Decrease
-TRAJECTORY_BUILDER_2D.max_range = 10. -- Decrease
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 50 -- Decrease
+TRAJECTORY_BUILDER_2D.max_range = 5. -- Decrease
 
 -------------------------------------------------------------------------------------
 
